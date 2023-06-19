99ea7d32870d573bdafce32a8683ef9d755b9fa9
Mai Van Sang <vansang1532000@gmail.com>
2023-06-19 15:27:38 +0700

    edit column customer product

--- models/customerProduct.model.js
+++ models/customerProduct.model.js
@@ -10,8 +10,8 @@ try {
 }
 // Schema
 const customerProductSchema = new mongoose.Schema({
-    customer: [{ type: Schema.Types.ObjectId, ref: 'customerModel' }], //ref tới model
-    product: [{ type: Schema.Types.ObjectId, ref: 'productModel' }],
+    customer_id: [{ type: Schema.Types.ObjectId, ref: 'customerModel' }], //ref tới model
+    product_id: [{ type: Schema.Types.ObjectId, ref: 'productModel' }],
     status: Number //1: chờ xử lý, 2: đang xử lý, 3: hoàn thành, 4: huỷ
 
 },
