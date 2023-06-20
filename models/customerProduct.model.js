55fe3d56c2f70abc2fd4adad6077a4a539d96931
Mai Van Sang <vansang1532000@gmail.com>
2023-06-20 13:32:36 +0700

    update: show customer wait from newest to oldest

--- models/customerProduct.model.js
+++ models/customerProduct.model.js
@@ -12,7 +12,15 @@ try {
 const customerProductSchema = new mongoose.Schema({
     customer_id: [{ type: Schema.Types.ObjectId, ref: 'customerModel' }], //ref tới model
     product_id: [{ type: Schema.Types.ObjectId, ref: 'productModel' }],
-    status: Number //1: chờ xử lý, 2: đang xử lý, 3: hoàn thành, 4: huỷ
+    status: Number, //1: chờ xử lý, 2: đang xử lý, 3: hoàn thành, 4: huỷ
+    timeCreate: {
+        type: Date,
+        default: Date.now
+    },
+    timeUpdate: {
+        type: Date,
+        default: Date.now
+    }
 
 },
 {collection: 'customer_product'}
